a3ea0e0576d75298d446898e92de55baed00544a
BuildTools <unconfigured@null.spigotmc.org>
2016-02-25 20:55:57 -0800

    There's the script
    ok.

---
+++ rps.js
@@ -0,0 +1,130 @@
+// Comments:
+// Note: You can use you browsers Console menu to play if nothing shows up
+// Note: This is still a work in progress
+
+
+// Where all the variables are contained, if edited the game is unusable More vars at the bottom of script
+var userInput = prompt("Rock, Paper or Scissors?")
+
+var computerInput = Math.random()
+
+var winner = "Undefined(Error with code.)"
+
+var reloadTempScript = "Reload Script Error"
+// End of variables
+
+// Computing the outcomes of the Computer
+if(computerInput < 0.33) {
+	computerInput = "Rock"
+}
+else if(computerInput < 0.65) {
+	computerInput = "Paper"
+}
+else
+{
+	computerInput = "Scissors"
+}
+// End of computer
+
+// Determine if computer or user won or was it a tie
+if(userInput.toUpperCase() === computerInput.toUpperCase()) {
+	winner = "It was a tie! So no one "
+}
+else if(userInput.toUpperCase() === "ROCK") {
+	if(computerInput === "Paper") {
+		winner = "Computer"
+	}
+	else
+	{
+		winner = "User(That's you!) "
+	}
+}
+else if(userInput.toUpperCase() === "PAPER") {
+	if(computerInput === "Rock") {
+		winner = "User(That's you!) "
+	}
+	else
+	{
+		winner = "Computer"
+	}
+}
+else if(userInput.toUpperCase() === "SCISSORS") {
+	if(computerInput === "Rock") {
+		winner = "Computer"
+	}
+	else {
+		winner = "User (That's you!)"
+	}
+}
+else if(userInput.toUpperCase() === "ROCKS") {
+	if(computerInput === "Paper") {
+		winner = "Computer"
+	}
+	else
+	{
+		winner = "User(That's you!) "
+	}
+}
+else if(userInput.toUpperCase() === "PAPERS") {
+	if(computerInput === "Rock") {
+		winner = "User(That's you!) "
+	}
+	else
+	{
+		winner = "Computer"
+	}
+}
+else if(userInput.toUpperCase() === "SCISSOR") {
+	if(computerInput === "Rock") {
+		winner = "Computer"
+	}
+	else {
+		winner = "User (That's you!)"
+	}
+}
+else if(userInput.toUpperCase() === "JASOR") {
+	winner = "User (Most likely Jasor if not you found my easter egg)"
+}
+else if(userInput.toUpperCase() === "BRIAN") {
+	winner = "User (Most likely Jasor if not you found my easter egg)"
+}
+else if(userInput.toUpperCase() === "EDDIE") {
+	winner = "User (Most likely Jasor if not you found my easter egg)"
+}
+else if(userInput.toUpperCase() === "MICHAEL") {
+	winner = "User (Most likely Jasor if not you found my easter egg)"
+}
+else if(userInput.toUpperCase() === "DERP") {
+	winner = "User (Most likely Jasor if not you found my easter egg)"
+}
+else {
+	winner = "No one"
+}
+// End of determining
+
+// Displaying results
+console.log("Computer Chose: " + computerInput + " " + "You Chose: " + userInput + "" + winner + " " + "has won the game")
+window.alert("Computer Chose: " + computerInput + " " + "You Chose: " + userInput + "" + winner + " " + "has won the game")
+// End of display
+
+//Reload Script More vars located here
+var reloadScript = function(reload) {
+	if(reload === true) {
+		//reloadTempScript = "To continue please reload the page"
+		window.location.reload(true);
+	}
+	else {
+		reloadTempScript = "You have chose not to continue, if you wish to continue reload the page."
+		console.log(reloadTempScript)
+		window.alert(reloadTempScript)
+	}
+}
+
+if(confirm("Do you want to keep playing?")) {
+	reloadScript(true)
+}
+else
+{
+	reloadScript(false)
+}
+// End Reload Script
