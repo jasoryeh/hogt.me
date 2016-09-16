0d9cdc99dc7fe68cede0924d5c345b180f50fd04
jasoryeh <jasoryeh@gmail.com>
2016-09-15 21:37:29 -0700

    Spacing...
    Fixed some sentence spacing and other stuff...

--- rps.js
+++ rps.js
@@ -4,13 +4,13 @@
 
 
 // Where all the variables are contained, if edited the game is unusable More vars at the bottom of script
-var userInput = prompt("Rock, Paper or Scissors?")
+var userInput = prompt("Please choose one of the following - Rock, Paper or Scissors?")
 
 var computerInput = Math.random()
 
-var winner = "Undefined(Error with code.)"
+var winner = "Undefined(Something bad happened... Report on https://github.com/jasoryeh/Rock-Paper-Scissos-JavaScript/issues)"
 
-var reloadTempScript = "Reload Script Error"
+var reloadTempScript = "Reload Script Error(Something bad happened... Report on https://github.com/jasoryeh/Rock-Paper-Scissos-JavaScript/issues)"
 // End of variables
 
 // Computing the outcomes of the Computer
@@ -28,77 +28,77 @@ else
 
 // Determine if computer or user won or was it a tie
 if(userInput.toUpperCase() === computerInput.toUpperCase()) {
-	winner = "It was a tie! So no one "
+	winner = "It was a tie! So no one has won. Try again =)!"
 }
 else if(userInput.toUpperCase() === "ROCK") {
 	if(computerInput === "Paper") {
-		winner = "Computer"
+		winner = " Computer"
 	}
 	else
 	{
-		winner = "User(That's you!) "
+		winner = " User(That's you!) "
 	}
 }
 else if(userInput.toUpperCase() === "PAPER") {
 	if(computerInput === "Rock") {
-		winner = "User(That's you!) "
+		winner = " User(That's you!) "
 	}
 	else
 	{
-		winner = "Computer"
+		winner = " Computer"
 	}
 }
 else if(userInput.toUpperCase() === "SCISSORS") {
 	if(computerInput === "Rock") {
-		winner = "Computer"
+		winner = " Computer"
 	}
 	else {
-		winner = "User (That's you!)"
+		winner = " User (That's you!)"
 	}
 }
 else if(userInput.toUpperCase() === "ROCKS") {
 	if(computerInput === "Paper") {
-		winner = "Computer"
+		winner = " Computer"
 	}
 	else
 	{
-		winner = "User(That's you!) "
+		winner = " User(That's you!) "
 	}
 }
 else if(userInput.toUpperCase() === "PAPERS") {
 	if(computerInput === "Rock") {
-		winner = "User(That's you!) "
+		winner = " User(That's you!) "
 	}
 	else
 	{
-		winner = "Computer"
+		winner = " Computer"
 	}
 }
 else if(userInput.toUpperCase() === "SCISSOR") {
 	if(computerInput === "Rock") {
-		winner = "Computer"
+		winner = " Computer"
 	}
 	else {
-		winner = "User (That's you!)"
+		winner = " User (That's you!)"
 	}
 }
 else if(userInput.toUpperCase() === "JASOR") {
-	winner = "User (Most likely Jasor if not you found my easter egg)"
+	winner = " User (Most likely Jasor if not you found my easter egg) "
 }
 else if(userInput.toUpperCase() === "BRIAN") {
-	winner = "User (Most likely Jasor if not you found my easter egg)"
+	winner = " User (Most likely Jasor if not you found my easter egg) "
 }
 else if(userInput.toUpperCase() === "EDDIE") {
-	winner = "User (Most likely Jasor if not you found my easter egg)"
+	winner = " User (Most likely Jasor if not you found my easter egg "
 }
 else if(userInput.toUpperCase() === "MICHAEL") {
-	winner = "User (Most likely Jasor if not you found my easter egg)"
+	winner = " User (Most likely Jasor if not you found my easter egg) "
 }
 else if(userInput.toUpperCase() === "DERP") {
-	winner = "User (Most likely Jasor if not you found my easter egg)"
+	winner = " User (Most likely Jasor if not you found my easter egg) "
 }
 else {
-	winner = "No one"
+	winner = " No one "
 }
 // End of determining
 
