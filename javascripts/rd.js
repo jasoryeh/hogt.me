9a1f16c7c657c1f08673d9c7c35c8ef22cabf50c
Jason <jasoryeh@gmail.com>
2016-11-28 18:33:05 -0800

    Update rd.js

--- javascripts/rd.js
+++ javascripts/rd.js
@@ -1,3 +1,3 @@
 
 var currentLink = window.location.href
-document.getElementsByTagName('head').write('<meta http-equiv="refresh" content="0; URL=\'' + currentLink + '\'\" />')
+document.getElementsByTagName('head')[0].write('<meta http-equiv="refresh" content="0; URL=\'' + currentLink + '\'\" />')
