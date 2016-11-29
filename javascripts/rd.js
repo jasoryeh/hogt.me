22509a379619f402dd2a366b1db842c320149922
Jason <jasoryeh@gmail.com>
2016-11-28 18:38:12 -0800

    Update rd.js

--- javascripts/rd.js
+++ javascripts/rd.js
@@ -1,3 +1,4 @@
 
 var currentLink = window.location.href;
+var newLink = currentLink.replace("jyeh.tk", "jasoryeh.tk"));
 document.getElementsByTagName('head')[0].write('<meta http-equiv="refresh" content="0; URL=\'' + currentLink + '\'\" />');
